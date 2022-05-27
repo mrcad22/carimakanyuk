d8a83f246cf2f9023e2b9e220d1ff6c146ed2d34
Muhammad Rosyad Syamil <42128450+mrcad22@users.noreply.github.com>
2022-05-28 04:17:50 +0700

    Adding Geolocation to locate user (High Accuracy)

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -108,6 +108,19 @@
                 })
             );
 
+            // Add geolocate control to the map.
+            map.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                    enableHighAccuracy: true
+                    },
+                    // When active the map will receive updates to the device's location as it changes.
+                    trackUserLocation: true,
+                    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                    showUserHeading: true
+                })
+            );
+
             const loadLocations = (geoJson) => {
                 geoJson.features.forEach((location) => {
                     const {geometry, properties} = location
